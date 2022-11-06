a94cabc178a8bee2c00653d8ceb07f20f3107ef9
olechka27 <olechka.dobrova@gmail.com>
2022-11-06 18:18:10 +0500

    animal

---
+++ src/Amphibian.java
@@ -0,0 +1,55 @@
+import java.util.Objects;
+
+public class Amphibian extends animals.Animal {
+
+    private String habitat;
+
+    public Amphibian(String name, int age, String habitat) {
+        super(name, age);
+        this.habitat = habitat;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("I`m eating");
+    }
+
+    @Override
+    public void go() {
+        System.out.println("I`m going");
+    }
+
+    public void hunt() {
+        System.out.println("I`m hunting");
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String h) {
+        this.habitat = ((h == null) || (h.isEmpty()) || (h.isBlank()))
+                ? NO_INFO
+                : h;
+    }
+
+    @Override
+    public String toString() {
+        return "Amphibian: " +
+                getName() +
+                ", age - " + getAge() +
+                ", habitat - " + getHabitat();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Amphibian predator = (Amphibian) o;
+
+        return Objects.equals(getName(), predator.getName())
+                && Objects.equals(getAge(), predator.getAge())
+                && Objects.equals(getHabitat(), predator.getHabitat());
+    }
+}
---
+++ src/Herbivore.java
@@ -0,0 +1,54 @@
+import java.util.Objects;
+
+public class Herbivore extends animals.mammals.Mammal {
+
+    private String foodType;
+
+    public Herbivore(String name, int age, String habitat, int moveSpeed, String foodType) {
+        super(name, age, habitat, moveSpeed);
+        setFoodType(foodType);
+    }
+
+    public void graze() {
+        System.out.println("I`m grazing!");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("I`m eating " + getFoodType());
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String ft) {
+        this.foodType = ((ft == null) || (ft.isEmpty()) || (ft.isBlank()))
+                ? NO_INFO
+                : ft;
+    }
+
+    @Override
+    public String toString() {
+        return "Herbivore: " +
+                getName() +
+                ", age - " + getAge() +
+                ", habitat - " + getHabitat() +
+                ", move speed - " + getMoveSpeed() +
+                ", food type - " + getFoodType();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Herbivore predator = (Herbivore) o;
+
+        return Objects.equals(getFoodType(), predator.getFoodType())
+                && Objects.equals(getName(), predator.getName())
+                && Objects.equals(getAge(), predator.getAge())
+                && Objects.equals(getHabitat(), predator.getHabitat())
+                && Objects.equals(getMoveSpeed(), predator.getMoveSpeed());
+    }
+}
